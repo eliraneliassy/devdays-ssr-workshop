a95f502b615b28bd9ea70705ecb97affecef4dcf
Eliran Eliassy <eliran.eliassy@gmail.com>
2019-05-14 14:50:45 +0300

    add title and meta services to prodcut component

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Item } from '../item';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-product',
@@ -10,10 +11,26 @@ import { Item } from '../item';
 export class ProductComponent implements OnInit {
 
   item: Item;
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private title: Title,
+    private meta: Meta) { }
 
   ngOnInit() {
     this.route.data.subscribe((res: any) => this.item = res.item);
+
+    this.title.setTitle('My Awesome Website' + this.item.description);
+
+
+    const fbTags = [];
+    fbTags.push({ name: 'og:title', content: this.item.description });
+    fbTags.push({ name: 'og:image', content: this.item.imageUrl });
+    fbTags.push({ name: 'og:site_name', content: 'Fake MarketPlace' });
+    fbTags.push({ name: 'og:description', content: this.item.description });
+
+    this.meta.addTags(fbTags);
+
+
   }
 
 }
